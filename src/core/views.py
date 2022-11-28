f0749d20f35bbb90d3f8add22b741e932e128fe5
Hyacinthe <hyacinthemiamo5@gmail.com>
2022-11-28 02:47:42 +0100

    Update views.py

--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+fron django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+  HttpResponse("<h2>Welcome to the Social Media platform</h2>")
