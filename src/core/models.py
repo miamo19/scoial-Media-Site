8a001a811ad6774055927ac147e05000e844e075
Hyacinthe <hyacinthemiamo5@gmail.com>
2022-12-03 10:29:53 +0100

    Update models.py

--- src/core/models.py
+++ src/core/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 class Profile(models.Model):
   name = models.CharField(max_lenght=50, blank=True)
+  contact = models.DecimalField(max_digit = 10, blank =True, null= True)
 
 # Create your models here.
