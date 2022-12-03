fe0fa480171d83c80249cbe3d75bfc8c8d66a332
Hyacinthe <hyacinthemiamo5@gmail.com>
2022-12-03 10:34:14 +0100

    Update admin.py

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Profile
 
-
 # Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+  
+#admin.site.register(Profile)
