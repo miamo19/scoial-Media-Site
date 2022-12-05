87cc3a4fe8eedf6f17f5efd5ca69eab3a390bc49
Hyacinthe <hyacinthemiamo5@gmail.com>
2022-12-05 03:30:54 +0100

    Update models.py

--- src/core/models.py
+++ src/core/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 class Profile(models.Model):
+  user = models.ForeighKeg(User, on_delete = models.CASCADE)
   name = models.CharField(max_lenght=50, blank=True)
   contact = models.DecimalField(max_digit = 10, blank =True, null= True)
+  
 
 # Create your models here.
